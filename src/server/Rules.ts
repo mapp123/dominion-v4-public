04ce064903ae69c46b308f93a1991f444a3f40bf
Charles Toller <charles@toller.dev>
2019-08-05 19:41:16 -0700

    Force colony during propserity and disable travis

--- src/server/Rules.ts
+++ src/server/Rules.ts
@@ -6,7 +6,7 @@ export default class Rules {
         const inProsperity = chosenCards.filter((a) => {
             return prosperityCards.includes(a);
         });
-        if (Math.random() * 10 < inProsperity.length) {
+        if (Math.random() * 10 < inProsperity.length || process.env.FORCE_COLONY === 'true') {
             return ['copper', 'silver', 'gold', 'platinum', 'estate', 'duchy', 'province', 'colony', 'curse', ...chosenCards];
         }
         return ['copper', 'silver', 'gold', 'estate', 'duchy', 'province', 'curse', ...chosenCards];

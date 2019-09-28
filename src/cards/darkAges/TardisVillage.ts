05aef0cb3cf69c449eba9133596eb03e15083e7b
Charles Toller <charles@toller.dev>
2019-09-27 22:48:46 -0600

    Create TardisVillage.ts

---
+++ src/cards/darkAges/TardisVillage.ts
@@ -0,0 +1,21 @@
+import Card from "../Card";
+import Player from "../../server/Player";
+import {Texts} from "../../server/Texts";
+import Util from "../../Util";
+
+export default class TardisVillage extends Card {
+    intrinsicTypes = ["action"] as const;
+    name = "tardis village";
+    cost = {
+        coin: 4
+    };
+    cardText = "+2 Actions\n" +
+        "Discard everything in play.";
+    supplyCount = 10;
+    cardArt = "/img/card-img/Mountain_VillageArt.jpg";
+    async onAction(player: Player): Promise<void> {
+        player.data.actions += 2;
+        await player.discard(player.data.playArea.slice(0), true);
+        player.data.playArea = [];
+    }
+}

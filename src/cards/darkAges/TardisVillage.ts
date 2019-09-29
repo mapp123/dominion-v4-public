dba4d7757aef690e7f162e9c66ac1eadbfcab450
Charles Toller <charles@toller.dev>
2019-09-29 17:46:16 -0600

    Remove tardis village from randomizer

--- src/cards/darkAges/TardisVillage.ts
+++ src/cards/darkAges/TardisVillage.ts
@@ -11,6 +11,7 @@ export default class TardisVillage extends Card {
         "Discard everything in play.";
     supplyCount = 10;
     cardArt = "/img/card-img/Mountain_VillageArt.jpg";
+    randomizable = false;
     async onAction(player: Player): Promise<void> {
         player.data.actions += 2;
         await player.discard(player.data.playArea.slice(0), true);
